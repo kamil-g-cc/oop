468854d8bc16437887b2193790e82295c2608da8
Kamil Grondys <kamil.grondys@codecool.com>
2020-05-10 14:30:11 +0200

    dodanie klasy
    Stworzenie klasy Samochod

--- Program.cs
+++ Program.cs
@@ -9,6 +9,23 @@ namespace oop
 	  public string Napisz(){
 	       return "(" + x + "," + y + ")";
 	  }
+	  public void Hello(){
+	       Console.WriteLine("Hello!");
+	  }
+    }
+
+    public class Car
+    {
+	    private string marka;
+	    private string model;
+	    public Car(string marka, string model, int rok_produkcji){
+	    	this.marka = marka;
+		this.model = model + rok_produkcji;
+	    }
+	    public string Napisz(){
+		    return marka + " " + model;
+	    }
+
     }
     
     class Program
@@ -18,6 +35,9 @@ namespace oop
              var a = 5;
 	     var b = 3;
 	     var punkt = new Point();
+	     var pierwszy_samochod = new Car("Ford", "Fiesta", 89);
+	     punkt.Hello();
+	     Console.WriteLine("moj pierwszy samochod to: " + pierwszy_samochod.Napisz());
 	     punkt.x = 3;
 	     punkt.y = 7;
 	     Console.WriteLine("Hello World! "+ (a+b)+" "+ punkt.Napisz());
