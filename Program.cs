ab9fb8868a3a143fd363bc1e33542029ba37070e
Kamil Grondys <kamil.grondys@codecool.com>
2020-05-10 14:14:53 +0200

    dodanie struktury
    Dodanie struktry Punkt

--- Program.cs
+++ Program.cs
@@ -2,11 +2,22 @@
 
 namespace oop
 {
+    public struct Point
+    {
+	  public double x;
+	  public double y;
+    }
+    
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+             var a = 5;
+	     var b = 3;
+	     var punkt = new Point();
+	     punkt.x = 3;
+	     punkt.y = 7;
+	     Console.WriteLine("Hello World! "+ (a+b)+" ("+punkt.x+","+punkt.y+")");
         }
     }
 }
