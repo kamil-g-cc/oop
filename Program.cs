1037a4f66f7f642e402b6366e4d7020fe194a097
Kamil Grondys <kamil.grondys@codecool.com>
2020-05-10 14:50:17 +0200

    zabawy z samochodem
    Rozbudowanie klasy Samochod

--- Program.cs
+++ Program.cs
@@ -19,13 +19,32 @@ namespace oop
 	    private string marka;
 	    private string model;
 	    private int rok_produkcji;
-	    public Car(string marka, string model, int rok_produkcji){
+	    private string color;
+	    public Car(string marka, string model, int rok_produkcji, string color)
+	    {
 	    	this.marka = marka;
 		this.model = model;
 		this.rok_produkcji = rok_produkcji;
+		this.color = color;
+	    }
+	    /*
+	     *   Konstruktor do tworzenia obiektu samochod, ktory jest czerwonym Fordem
+	     *
+	     */
+	    public Car(string model){
+	    	this.model = model;
+		marka = "Ford";
+		color = "czerwony";
+		rok_produkcji = 2020;
+	    }
+	    public Car(){
+                marka = "Fiat";
+    		model = "500";
+		color = "czerwony";
+ 		rok_produkcji = 2002;
 	    }
 	    public string Napisz(){
-		    return marka + " " + model+ " " + rok_produkcji;
+		    return color + " " + marka + " " + model+ " " + rok_produkcji;
 	    }
 
     }
@@ -37,7 +56,7 @@ namespace oop
              var a = 5;
 	     var b = 3;
 	     var punkt = new Point();
-	     var pierwszy_samochod = new Car("Ford", "Fiesta", 89);
+	     var pierwszy_samochod = new Car("Ford", "Fiesta", 89, "zielony");
 	     punkt.Hello();
 	     Console.WriteLine("moj pierwszy samochod to: " + pierwszy_samochod.Napisz());
 	     punkt.x = 3;
